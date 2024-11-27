f0eb1260d8b9050698365c26d4010b95b6b135aa
gili12314 <gili12314@gmail.com>
2024-11-27 15:20:40 +0200

    Gils - Add new post api

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,13 @@
 const PostModel = require("../models/posts_model");
 
+const createPost = async (req, res) => {
+    console.log("Post create");
+    try {
+        const post = await PostModel.create(req.body);
+        res.status(201).send(post);
+    } catch (err) { res.status(400).send(err.message); }
+}
+
 module.exports = {
+    createPost
 };
