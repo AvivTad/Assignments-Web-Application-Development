5c9db3c2103f3c1fd3a4a47ad75531be7e0a2fa5
Avivtad <avivitv@gmail.com>
2024-11-28 17:14:09 +0200

    Get All Posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -9,19 +9,13 @@ const createPost = async (req, res) => {
 }
 
 const getAllPosts = async (req, res) => {
-    const filter = req.query.owner;
-    try {
-      if (filter) {
-        const posts = await PostModel.find({ owner: filter });
-        res.send(posts);
-      } else {
-        const posts = await PostModel.find();
-        res.send(posts);
-      }
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  };
+  try {
+    const posts = await PostModel.find();
+    res.send(posts);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
   
 
 module.exports = {
