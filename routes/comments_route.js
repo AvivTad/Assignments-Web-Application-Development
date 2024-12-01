b893c592de98a4741da702e0dee40c602ec74c45
Avivtad <avivitv@gmail.com>
2024-12-01 17:21:31 +0200

    update comments

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -4,5 +4,7 @@ const commentsController = require("../controllers/comments_controller");
 
 router.post("/", commentsController.createComment);
 router.get("/", commentsController.getComments);
+router.put("/:id", commentsController.updateComment);
+
 
 module.exports = router;
