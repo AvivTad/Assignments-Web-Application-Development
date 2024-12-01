e9090ef92dcff8a78cce61a943630a0785e1d57b
gili12314 <gili12314@gmail.com>
2024-12-01 17:33:34 +0200

    Gils - Add delete comment by id api

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -5,6 +5,7 @@ const commentsController = require("../controllers/comments_controller");
 router.post("/", commentsController.createComment);
 router.get("/", commentsController.getComments);
 router.put("/:id", commentsController.updateComment);
+router.delete("/:id", commentsController.deleteComment);
 
 
 module.exports = router;
