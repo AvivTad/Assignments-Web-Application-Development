e76a977211184457f3c43634526cf139303258e1
gili12314 <gili12314@gmail.com>
2024-12-01 17:10:36 +0200

    Gils - add get comments + comments by post id

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const commentsController = require("../controllers/comments_controller");
 
 router.post("/", commentsController.createComment);
-// router.get("/:postId", commentsController.getCommentsByPostId);
+router.get("/", commentsController.getComments);
 
 module.exports = router;
